0288d3e146cea5d53f72ec29e1de3b17b96a3f79
lamoncomputingg <lamonfullstack2022@gmail.com>
2023-10-01 20:20:11 -0300

    inserimento de código no arquivo MinhaClasse.java

--- java-anatomia de classes/src/MinhaClasse.java
+++ java-anatomia de classes/src/MinhaClasse.java
@@ -1,3 +1,9 @@
+/*
+Todo Arquivo java tem que estar dentro de uma pasta que é chamada de src,
+onde o Arquivos tem que ter as Iniciais em caixa alta
+*/
 public class MinhaClasse {
-    
+    public static void main(String [] args){
+        System.out.print("=)");
+    }
 }
