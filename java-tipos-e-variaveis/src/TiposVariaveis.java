f8266ed7cdcff10fee60de6d9032e59367f1d923
lamoncomputingg <lamonfullstack2022@gmail.com>
2023-10-06 21:07:44 -0300

    Estudos sobre variaveis.

---
+++ java-tipos-e-variaveis/src/TiposVariaveis.java
@@ -0,0 +1,22 @@
+public class TiposVariaveis{
+    public static void main(String[] arg) throws Exception{
+        //Estudar Tipos Primitivos;
+        //Estudar a Classe String que Representa texto na aplicação.
+
+        String meuNome = "LAMON SANTOS";
+
+        double salarioMinimo = 2500; /*2500 = dois mil e quinhetos, 2.500 é dois ponto cinco*/
+    
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        int numero = 5;
+
+        numero = 10;
+
+        System.out.print(numero);
+
+        final double VALOR_DE_PI = 3.14;
+    }
+}
