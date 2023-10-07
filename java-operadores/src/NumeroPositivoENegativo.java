b155e86ed684c15dcd18bc04c13febcf8fb7999a
lamoncomputingg <lamonfullstack2022@gmail.com>
2023-10-06 21:44:58 -0300

    Implementação do código de NumeroPositivoENegativo

---
+++ java-operadores/src/NumeroPositivoENegativo.java
@@ -0,0 +1,9 @@
+public class NumeroPositivoENegativo {
+    public static void main(String[] args){
+        int numero = 5;
+        numero = -5;
+        System.out.println(numero);
+        numero = numero * -1;
+        System.out.println(numero);
+    }
+}
