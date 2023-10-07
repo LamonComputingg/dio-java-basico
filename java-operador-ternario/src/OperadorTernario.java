ae45a5ac2e99dc231fe8c813143de84b36bbb02c
lamoncomputingg <lamonfullstack2022@gmail.com>
2023-10-07 07:29:36 -0300

    código implementado

---
+++ java-operador-ternario/src/OperadorTernario.java
@@ -0,0 +1,14 @@
+public class OperadorTernario {
+    public static void manin(String[] args){
+
+        int valorPrimeiro, valorSegundo;
+
+        valorPrimeiro = 5;
+        valorSegundo = 6;
+
+        int resultadoDosDOis = valorPrimeiro == valorSegundo ? 1 : 0;
+
+        System.out.println(resultadoDosDOis);
+
+    }
+}
