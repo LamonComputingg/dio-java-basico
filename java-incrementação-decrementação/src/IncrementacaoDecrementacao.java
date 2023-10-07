320e6ad60ae9d5598fb5c4f9b81ef823939576e2
lamoncomputingg <lamonfullstack2022@gmail.com>
2023-10-07 07:07:44 -0300

    projeto de incrementação e decrementa~ção criado.

---
+++ java-incrementação-decrementação/src/IncrementacaoDecrementacao.java
@@ -0,0 +1,8 @@
+public class IncrementacaoDecrementacao {
+    public static void main(String[] args){
+        int numeroInicial = 10;
+        //X repetição
+        System.out.println(++numeroInicial); //Atribuição para mais um;
+        System.out.println(--numeroInicial); //Decrementação de menos um; 
+    }
+}
