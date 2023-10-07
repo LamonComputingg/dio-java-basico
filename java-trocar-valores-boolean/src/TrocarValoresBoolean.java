cf23966c40778bd3834d8142558604bc13bde2bb
lamoncomputingg <lamonfullstack2022@gmail.com>
2023-10-07 07:15:35 -0300

    arquivo com código sobre troca de valores boolean

---
+++ java-trocar-valores-boolean/src/TrocarValoresBoolean.java
@@ -0,0 +1,9 @@
+public class TrocarValoresBoolean {
+    public static void main(String[] args){
+        boolean variavel = true;
+        variavel = !variavel; //o silan de ! na frente da varavel trocar o valor dela de verdadeiro para falso
+        System.out.println(variavel);
+        variavel = !variavel;
+        System.out.println(variavel); //o silan de ! na frente da varavel trocar o valor dela de verdadeiro para falso
+    }
+}
