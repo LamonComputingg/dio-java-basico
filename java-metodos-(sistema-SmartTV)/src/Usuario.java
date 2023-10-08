8af8ab0896268b0f2a96b1c44bf0ad71a4385d77
lamoncomputingg <lamonfullstack2022@gmail.com>
2023-10-08 14:44:55 -0300

    incrementação dos metodos: aumentar volume e diminuir volume no arquivo usuario.

--- java-metodos-(sistema-SmartTV)/src/Usuario.java
+++ java-metodos-(sistema-SmartTV)/src/Usuario.java
@@ -2,6 +2,13 @@ public class Usuario {
     public static void main(String[] args){
         SmartTv smartTv = new SmartTv();
 
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.aumentarVolume();
+
+        System.out.println("Volume atual: " + smartTv.volume);
+
         System.out.println("Tv Ligada ? " + smartTv.ligada);
         System.out.println("Canal Atual: " + smartTv.canal);
         System.out.println("Volume: " + smartTv.volume);
