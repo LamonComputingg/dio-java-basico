f381b6fe7319710f045a94666b1879b53b1f3de2
lamoncomputingg <lamonfullstack2022@gmail.com>
2023-10-08 15:06:10 -0300

    organização de código no arquivo usuario.

--- java-metodos-(sistema-SmartTV)/src/Usuario.java
+++ java-metodos-(sistema-SmartTV)/src/Usuario.java
@@ -1,21 +1,19 @@
 public class Usuario {
     public static void main(String[] args){
         SmartTv smartTv = new SmartTv();
-
+        smartTv.ligar();
         smartTv.diminuirVolume();
         smartTv.diminuirVolume();
         smartTv.diminuirVolume();
         smartTv.aumentarVolume();
-        
-        System.out.println("Volume atual: " + smartTv.volume);
-        smartTv.ligar();
-        System.out.println("Tv Ligada ? " + smartTv.ligada);
+        smartTv.mudarCanal(19);
         System.out.println("Canal Atual: " + smartTv.canal);
+        System.out.println("Volume atual: " + smartTv.volume);
+        System.out.println("Tv Ligada ? " + smartTv.ligada);
         System.out.println("Volume: " + smartTv.volume);
-
         smartTv.ligar();
-        System.out.println("Novo Status da TV -> Ligada? " + smartTv.ligada); //10:42 assistir aula
+        System.out.println("Status da TV -> Ligada? " + smartTv.ligada); //10:42 assistir aula
         smartTv.desligar();
-        System.out.println("Novo Status da TV -> Ligada? " + smartTv.ligada);
+        System.out.println("Novo status da TV -> Ligada? " + smartTv.ligada);
     }
 }
