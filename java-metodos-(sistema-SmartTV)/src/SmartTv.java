c81b6d9572f1007c0a67fdb9a07353e349f52274
lamoncomputingg <lamonfullstack2022@gmail.com>
2023-10-08 15:02:59 -0300

    inserimento do código completo no metodo mudar canal.

--- java-metodos-(sistema-SmartTV)/src/SmartTv.java
+++ java-metodos-(sistema-SmartTV)/src/SmartTv.java
@@ -4,9 +4,8 @@ public class SmartTv {
         int volume =25;
 
         public void mudarCanal(int novoCanal){
-            
+            canal = novoCanal;
         }
-
         public void aumentarCanal(){
             // canal = canal + 1;
             canal++;
