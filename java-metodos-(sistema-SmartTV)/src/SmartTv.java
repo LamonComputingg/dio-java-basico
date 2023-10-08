01c0c442455996b290ad27d7b86748939f560d37
lamoncomputingg <lamonfullstack2022@gmail.com>
2023-10-08 14:50:57 -0300

    mensagens em relação ao volume foram incrementadas

--- java-metodos-(sistema-SmartTV)/src/SmartTv.java
+++ java-metodos-(sistema-SmartTV)/src/SmartTv.java
@@ -6,10 +6,12 @@ public class SmartTv {
         public void aumentarVolume(){
             //volume = volume + 1;
             volume++;
+            System.out.println("Aumentando o volume para: " + volume);
         }
         public void diminuirVolume(){
             //volume = volume - 1;
             volume--;
+            System.out.println("Diminuindo o volume para: " + volume);
         }
         public void ligar(){
             ligada=true;
