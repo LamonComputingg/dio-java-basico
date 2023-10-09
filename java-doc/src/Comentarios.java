19462bf1e2965fd2101e806aa851836a6ac32154
lamoncomputingg <lamonfullstack2022@gmail.com>
2023-10-09 14:30:32 -0300

    inserimento do código do comentario.java

--- java-doc/src/Comentarios.java
+++ java-doc/src/Comentarios.java
@@ -1,3 +1,11 @@
 public class Comentarios {
-    
+    public static void main(String[] args){
+        //Olá, eu sou um comentário em uma única linha de código;
+
+        /*Olá,
+         * Eu sou um comenário
+         * que posso ser mais detalhado
+         * quando for necessário
+         */
+    }
 }
