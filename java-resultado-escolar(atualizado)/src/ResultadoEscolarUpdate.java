a6541e48cdadab8a41d08204fde917c312838838
lamoncomputingg <lamonfullstack2022@gmail.com>
2023-10-10 08:09:51 -0300

    código implementado

--- java-resultado-escolar(atualizado)/src/ResultadoEscolarUpdate.java
+++ java-resultado-escolar(atualizado)/src/ResultadoEscolarUpdate.java
@@ -1,3 +1,7 @@
 public class ResultadoEscolarUpdate {
-    
+    public static void main(String[] args){
+        int nota = 5;
+        String resultado = nota >= 7 ? "Aprovado" : "Reprovado";
+        System.out.println(resultado);
+    }
 }
