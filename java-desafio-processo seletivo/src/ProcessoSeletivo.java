02215373b282d0a688d7c7a07526fedecb0c4517
lamoncomputingg <lamonfullstack2022@gmail.com>
2023-10-15 13:48:04 -0300

    paramentro sobre salariopretendido implementado

--- java-desafio-processo seletivo/src/ProcessoSeletivo.java
+++ java-desafio-processo seletivo/src/ProcessoSeletivo.java
@@ -1,5 +1,8 @@
 public class ProcessoSeletivo{
     public static void main(String[] args){
-        System.out.println("");
+        System.out.println("Processo Seletivo");
+    }
+    static void analisarCandidato(double salarioPretendido){
+
     }
 }
