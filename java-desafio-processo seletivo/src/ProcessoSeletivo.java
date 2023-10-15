a255ed6a0656cc825fad3cbf23735f119aed1af7
lamoncomputingg <lamonfullstack2022@gmail.com>
2023-10-15 14:16:05 -0300

    implementando código funcional

--- java-desafio-processo seletivo/src/ProcessoSeletivo.java
+++ java-desafio-processo seletivo/src/ProcessoSeletivo.java
@@ -1,3 +1,5 @@
+import java.util.concurrent.ThreadLocalRandom;
+
 public class ProcessoSeletivo{
     public static void main(String[] args){
         System.out.println("(Processo Seletivo)");
@@ -18,6 +20,16 @@ public class ProcessoSeletivo{
             "Silva",
             "Souza"
         };
+        int candidatosSelecionados = 0;
+        int candidatosAtual = 0;
+        double salarioBase = 2000.0;
+        while(candidatosSelecionados < 5){
+            String candidato = candidatos[candidatosAtual];
+            double salarioPretendido = valorPretendido();
+        }
+    }
+    static double valorPretendido(){
+        return ThreadLocalRandom.current().nextDouble(1800, 2200);
     }
     static void analisarCandidato(double salarioPretendido){
         double salarioBase = 2000.0;
