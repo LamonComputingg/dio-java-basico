f9daad0d680e6cf9a6ebc9b9776ce010cc32ffe9
lamoncomputingg <lamonfullstack2022@gmail.com>
2023-10-15 14:00:16 -0300

    condições estabelecidas

--- java-desafio-processo seletivo/src/ProcessoSeletivo.java
+++ java-desafio-processo seletivo/src/ProcessoSeletivo.java
@@ -1,14 +1,17 @@
 public class ProcessoSeletivo{
     public static void main(String[] args){
-        System.out.println("Processo Seletivo");
+        System.out.println("(Processo Seletivo)");
     }
     static void analisarCandidato(double salarioPretendido){
         double salarioBase = 2000.0;
         if(salarioBase > salarioPretendido){
-            System.out.println("LIGAR PARA O CANDIDATO!");
+            System.out.println("LIGAR PARA O CANDIDATO!!");
 
         }else if(salarioBase == salarioPretendido){
-            System.out.println("");
+            System.out.println("LIGAR PARA O CANDIDATO COM CONTRA PROPOSTA!!!");
+        }
+        else{
+            System.out.println("AGUARDANDO O RESULTADO DOS DEMAIS CANDIDATOS!!!!");
         }
     }
 }
