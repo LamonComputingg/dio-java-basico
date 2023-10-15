442e7d9c2a5545a99fd9d4a14cf6984d81af8381
lamoncomputingg <lamonfullstack2022@gmail.com>
2023-10-15 14:09:41 -0300

    array com candidatos

--- java-desafio-processo seletivo/src/ProcessoSeletivo.java
+++ java-desafio-processo seletivo/src/ProcessoSeletivo.java
@@ -1,9 +1,23 @@
 public class ProcessoSeletivo{
     public static void main(String[] args){
         System.out.println("(Processo Seletivo)");
-        analisarCandidato(1900.0);
-        analisarCandidato(2200.0);
-        analisarCandidato(2000.0);
+        //analisarCandidato(1900.0);
+        //analisarCandidato(2200.0);
+        //analisarCandidato(2000.0);
+    }
+    static void selecaoCandidatos(){
+        String [] candidatos = {
+            "Lamon",
+            "Limon",
+            "Santos",
+            "Karina",
+            "Florentino",
+            "Carmelia",
+            "Laerson",
+            "Romão",
+            "Silva",
+            "Souza"
+        };
     }
     static void analisarCandidato(double salarioPretendido){
         double salarioBase = 2000.0;
